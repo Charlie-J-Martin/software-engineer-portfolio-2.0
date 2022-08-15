bdc29a34a5108d0d33cd8fc0c12ae138e26aa4e1
Charlie Martin <charliejm7@gmail.com>
2022-08-15 15:43:54 +0100

    feat: Implemenet navbar and mobile menu

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+
+const Sidebar = ({ isOpen, toggle }) => {
+  let customStyle = {};
+
+  if (isOpen) {
+    customStyle = {
+      opacity: '100%',
+      top: '0%',
+    };
+  }
+
+  return (
+    <>
+      <div
+        className='sidebar-container'
+        isOpen={isOpen}
+        onClick={toggle}
+        style={customStyle}
+      >
+        <div className='icon' onClick={toggle}>
+          <FaTimes className={'close-icon'} />
+        </div>
+        <div className='sidebar-wrapper'>
+          <ul className='sidebar-menu'>
+            <a className='sidebar-link' href='#' onClick={toggle}>
+              Projects
+            </a>
+            <a className='sidebar-link' href='#' onClick={toggle}>
+              Values
+            </a>
+            <a className='sidebar-link' href='#' onClick={toggle}>
+              Contact Me
+            </a>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
