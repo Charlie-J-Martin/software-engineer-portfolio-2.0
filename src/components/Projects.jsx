04514255807c0738e542ed9305a5e554cd3b1c6e
Charlie Martin <charliejm7@gmail.com>
2022-08-25 21:10:01 +0100

    feat: added projects comp

---
+++ src/components/Projects.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Subheading from './Subheading';
+import { Project } from './Project';
+import { projectsData } from '../data/projectsData';
+
+function createProject(project) {
+  return (
+    <Project
+      key={project.id}
+      listId={project.id}
+      projectImage={project.projectImage}
+      name={project.name}
+      description={project.description}
+      github={project.github}
+      liveWebsite={project.liveWebsite}
+      technologiesUsed={project.technologiesUsed}
+    />
+  );
+}
+
+const Projects = () => {
+  return (
+    <>
+      <div className='projects-container'>
+        <Subheading number='02.' name='Projects' />
+        {projectsData.map(createProject)}
+      </div>
+    </>
+  );
+};
+
+export default Projects;
