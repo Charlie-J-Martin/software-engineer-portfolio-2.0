66f26591ce06a731c74fa8965f7bc0234f6bdd0b
Charlie Martin <charliejm7@gmail.com>
2022-09-11 11:16:11 +0100

    Added animations around text segments and minor style changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -17,16 +17,23 @@ const HeroSection = () => {
         </div>
         <div className='hero-content'>
           <div className='hero-words'>
-          <h1>Hi, my name is Charlie.</h1>
-          <h2>Nice to meet you.</h2>
-          <p>
-            I’m a backend software engineer who loves to solve problems and
-            making the impossible possible. I’m currently focused on building a
-            cutting edge backend system for supplying data at Elsevier.
-          </p>
+            <div className='container drawer'>
+              <h1>Hi, my name is Charlie.</h1>
+              <h2>Nice to meet you.</h2>
+              <p>
+                I’m a backend software engineer who loves to solve problems and
+                making the impossible possible. I’m currently focused on
+                building a cutting edge backend system for supplying data at
+                Elsevier.
+              </p>
+            </div>
           </div>
           <div className='hero-image-container'>
-            <img className='hero-image' src='/img/profile.jpeg' alt='personal portrait' />
+            <img
+              className='hero-image'
+              src='/img/profile.jpeg'
+              alt='personal portrait'
+            />
             <div className='hero-image-background'></div>
           </div>
         </div>
