3d860c25484b288b6040423336e9513eacc12a5b
Charlie Martin <charliejm7@gmail.com>
2022-12-20 10:35:02 +0000

    feat: Added api project information

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,12 +1,22 @@
 export const projectsData = [
   {
-    name: 'Historical F1',
-    description: 'Coming soon...',
-    github: 'https://github.com/Charlie-J-Martin',
-    liveWebsite: 'https://www.formula1.com/',
-    technologiesUsed: ['TypeScript', 'React', 'NodeJS', 'GraphQL'],
-    projectImage: 'img/myProjects/formula-1.jpg',
-    projectImageAltTag: 'Red Bull formula 1 car racing in Silverstone',
+    name: 'Football Transfers API',
+    description:
+      'The Football Transfers API is built using football transfer data from the summer 2022 transfer window and includes all the transfers from the top leagues around the world. This API allows you to get data based on a variety of different parameters such as age, market value, fee paid and clubs to name a few.',
+    github:
+      'https://github.com/Charlie-J-Martin/football-summer-transfers-2022-api',
+    liveWebsite:
+      'https://github.com/Charlie-J-Martin/football-summer-transfers-2022-api',
+    technologiesUsed: [
+      'TypeScript',
+      'Node.js',
+      'MongoDB',
+      'JWTs',
+      'Jest',
+      'Supertest',
+    ],
+    projectImage: 'img/myProjects/football.jpg',
+    projectImageAltTag: 'A football being kicked',
   },
   {
     name: 'Amazon Price Checker',
