1c4a6b72ed533cc4693515a7be64dc275c8eb002
Charlie Martin <charliejm7@gmail.com>
2022-12-22 11:37:21 +0000

    feat: minor style changes and additional content

--- src/components/myProjects/Project.jsx
+++ src/components/myProjects/Project.jsx
@@ -14,30 +14,37 @@ export const Project = (props) => {
           />
           <div className='project-text'>
             <h1 className='project-name'>{props.name}</h1>
-            <p className='project-description'>{props.description}</p>
-            <div className='project-links'>
-              <a href={props.github} target='_blank' rel='noreferrer'>
-                <img
-                  className='project-link-icons github'
-                  src='img/github.svg'
-                  alt='The Github logo'
-                />
-              </a>
-              <a href={props.liveWebsite} target='_blank' rel='noreferrer'>
-                <img
-                  className='project-link-icons link'
-                  src='img/link.svg'
-                  alt='A chain link logo representing a external link'
-                ></img>
-              </a>
+            <div className='project-description'>
+              <p>{props.descriptionIntro}</p>
+              <br />
+              <p>{props.descriptionAdditional}</p>
             </div>
+
             <h6 className='technology-subtitle'>
               Technologies you'll find here:
             </h6>
-            <div className='technology-used'>
-              {props.technologiesUsed.map((technology) => (
-                <h6>{technology}</h6>
-              ))}
+            <div className='tech-links'>
+              <div className='technology-used'>
+                {props.technologiesUsed.map((technology) => (
+                  <h6>{technology}</h6>
+                ))}
+              </div>
+              <div className='project-links'>
+                <a href={props.github} target='_blank' rel='noreferrer'>
+                  <img
+                    className='project-link-icons github'
+                    src='img/github.svg'
+                    alt='The Github logo'
+                  />
+                </a>
+                <a href={props.liveWebsite} target='_blank' rel='noreferrer'>
+                  <img
+                    className='project-link-icons link'
+                    src='img/link.svg'
+                    alt='A chain link logo representing a external link'
+                  ></img>
+                </a>
+              </div>
             </div>
           </div>
         </AnimationOnScroll>
